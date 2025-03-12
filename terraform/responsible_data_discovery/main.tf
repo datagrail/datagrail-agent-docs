bdd37a4f8209cbe6713e7c48c997ea5683f8f63f
Taylor Graham <taylor.graham@datagrail.io>
2025-03-11 17:44:18 -0700

    removing ARNs

--- terraform/responsible_data_discovery/main.tf
+++ terraform/responsible_data_discovery/main.tf
@@ -12,5 +12,5 @@ module "rdd-agent" {
   agent_image_uri     = "338780525468.dkr.ecr.us-west-2.amazonaws.com/datagrail-rdd-agent:v0.7.3"
   cluster_arn = ""
   datagrail_api_key = var.datagrail_api_key
-  integration_credentials_arns = ["arn:aws:secretsmanager:us-west-2:158714794554:secret:datagrail-rdd-agent.snowflake-bjqVnb"]
+  integration_credentials_arns = [""]
 }
