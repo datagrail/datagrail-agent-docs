9cecb690a2462bb1a348e952d10c35265b6123c1
Taylor Graham <taylor.graham@datagrail.io>
2025-03-11 17:47:50 -0700

    fmt

--- terraform/responsible_data_discovery/main.tf
+++ terraform/responsible_data_discovery/main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region  = ""
+  region = ""
 }
 
 module "rdd-agent" {
   source = "./aws/ecs"
 
-  vpc_id              = ""
-  private_subnet_ids  = ["", ""]
-  datagrail_subdomain = "solutions.datagrail.io"
-  credentials_manager = "AWSSecretsManager"
-  agent_image_uri     = "338780525468.dkr.ecr.us-west-2.amazonaws.com/datagrail-rdd-agent:v0.7.3"
-  cluster_arn = ""
-  datagrail_api_key = var.datagrail_api_key
+  vpc_id                       = ""
+  private_subnet_ids           = ["", ""]
+  datagrail_subdomain          = "<your_subdomain>.datagrail.io"
+  credentials_manager          = "AWSSecretsManager"
   integration_credentials_arns = [""]
+  agent_image_uri              = "338780525468.dkr.ecr.us-west-2.amazonaws.com/datagrail-rdd-agent:v0.7.3"
+  cluster_arn                  = ""
+  datagrail_api_key            = var.datagrail_api_key
 }
--- terraform/responsible_data_discovery/variables.tf
+++ terraform/responsible_data_discovery/variables.tf
@@ -1,4 +1,4 @@
 variable "datagrail_api_key" {
   description = "API key for the agent from DataGrail app."
-  type = string
+  type        = string
 }
